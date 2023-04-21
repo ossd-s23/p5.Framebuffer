12a915c0086bdec5552ea5d23ec874d8ce947d6f
anpanring <jid9834@nyu.edu>
2023-04-21 18:52:40 -0400

    Control z-coord with arrow keys

--- examples/olympic/sketch.js
+++ examples/olympic/sketch.js
@@ -1,68 +1,93 @@
 let blurRenderer
+let s = 0.0;
+let a = 0.0;
+
+var z = 500;
+var easing = 0.05;
+var targetZ = 0
 
 function setup() {
-  createCanvas(1000, 500, WEBGL)
-  pixelDensity(2)
+  createCanvas(1000, 500, WEBGL);
+  pixelDensity(2);
   blurRenderer = createGaussianBlurRenderer({ antialias: true })
 }
 
 function draw() {
+  a = a + 0.02;
+  s = cos(a) * 2;
+
+  var dz = targetZ - z;
+  if(abs(dz) > 1) {
+    z += dz * easing;
+  }
+
   blurRenderer.draw(() => {
     clear()
     push()
-    background(255)
+    background('white')
     noStroke()
     lights()
- 
 
     push()
     fill('#51D2F1')
-    translate(0, 0, 0);
+    translate(-150, -45, z);
     rotateZ(frameCount * 0.01);
     rotateX(frameCount * 0.01);
-    rotateY(frameCount * 0.01);
+    // rotateY(frameCount * 0.01);
     torus(70, 10);
     pop()
 
     push()
     fill('#000000')
-    translate(150, 0, 0);
+    translate(0, -45, z);
     rotateZ(frameCount * 0.01);
-    rotateX(frameCount * 0.01);
+    // rotateX(frameCount * 0.01);
     rotateY(frameCount * 0.01);
     torus(70, 10);
     pop()
 
     push()
     fill('#EE4B4B')
-    translate(300, 0, 0);
+    translate(150, -45, z);
     rotateZ(frameCount * 0.01);
     rotateX(frameCount * 0.01);
-    rotateY(frameCount * 0.01);
+    // rotateY(frameCount * 0.01);
     torus(70, 10);
     pop()
 
     push()
     fill('#FAFA00')
-    translate(75, 90, 0);
+    translate(-75, 45, z);
     rotateZ(frameCount * 0.01);
-    rotateX(frameCount * 0.01);
+    // rotateX(frameCount * 0.01);
     rotateY(frameCount * 0.01);
     torus(70, 10);
     pop()
 
     push()
     fill('#72FA00')
-    translate(225, 90, 0);
-    rotateZ(frameCount * 0.01);
+    translate(75, 45, z);
+    // rotateZ(frameCount * 0.01);
     rotateX(frameCount * 0.01);
     rotateY(frameCount * 0.01);
     torus(70, 10);
     pop()
 
 
-   
-
     pop()
   })
+}
+
+function keyPressed() {
+  if(keyCode == UP_ARROW)
+  {
+    console.log('pressed');
+    targetZ = z + 200; 
+  }
+
+  if(keyCode == DOWN_ARROW)
+  {
+    console.log('pressed');
+    targetZ = z - 200; 
+  }
 }
