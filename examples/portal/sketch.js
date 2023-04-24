a9b593504fc47254d19d5db56a0958439e874206
anpanring <jid9834@nyu.edu>
2023-04-24 13:06:21 -0400

    olympic and portal

--- examples/portal/sketch.js
+++ examples/portal/sketch.js
@@ -79,11 +79,86 @@ function draw() {
     pop()
   })
 
-  clear();
-  texture(fbo.depth);
+  // clear();
+  // texture(fbo.depth);
+  // stroke('white');
+  // rotateX(millis() / 1000);
+  // box(width*0.5, height*0.5);
+
+  clear()
+  background("white");
+  push()
+  // background('white')
+  noStroke()
+  // lights()
+
+  push()
+  fill('#51D2F1')
+  translate(-150, -45, z);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  // rotateY(frameCount * 0.01);
+  torus(70, 10);
+  pop()
+
+  push()
+  fill('#000000')
   stroke('white');
-  rotateX(millis() / 1000);
-  box(width*0.5, height*0.5);
+  translate(0, -45, z);
+  rotateZ(frameCount * 0.01);
+  // rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  torus(70, 10);
+  pop()
+
+  push()
+  fill('#EE4B4B')
+  translate(150, -45, z);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  // rotateY(frameCount * 0.01);
+  torus(70, 10);
+  pop()
+
+  push()
+  fill('#FAFA00')
+  translate(-75, 45, z);
+  rotateZ(frameCount * 0.01);
+  // rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  torus(70, 10);
+  pop()
+
+  push()
+  fill('#72FA00')
+  texture(fbo.color);
+  translate(-75, 45, z);
+  rotateZ(frameCount * 0.01);
+  // rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  circle(0, 0, 100);
+  pop()
+
+  push()
+  fill('#72FA00')
+  translate(75, 45, z);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  // rotateY(frameCount * 0.01);
+  torus(70, 10);
+  pop()
+
+  push()
+  fill('#72FA00')
+  texture(fbo.color);
+  translate(75, 45, z);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  // rotateY(frameCount * 0.01);
+  circle(0, 0, 100);
+  pop()
+
+  pop()
 }
 
 function keyPressed() {
